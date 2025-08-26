41a4249645eb70769ff291717193fe15292bf883
Dwin <dwin@bupt.edu.cn>
2025-08-27 01:46:39 +0800

    增加日志信息

--- tlias-java-web/src/main/java/com/dwinovo/controller/DeptController.java
+++ tlias-java-web/src/main/java/com/dwinovo/controller/DeptController.java
@@ -1,6 +1,8 @@
 package com.dwinovo.controller;
 
 import java.util.List;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.dwinovo.pojo.Dept;
@@ -9,6 +11,7 @@ import com.dwinovo.service.DeptService;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+@Slf4j
 @RequestMapping("/depts")
 @RestController
 public class DeptController {
@@ -19,7 +22,7 @@ public class DeptController {
     @GetMapping
     public Result list()
     {
-        System.out.println("查询全部部门数据");
+        log.info("查询全部部门数据");
         List<Dept> deptList = deptService.findAll();
         return Result.success(deptList);
     }
@@ -28,7 +31,7 @@ public class DeptController {
      */
     @DeleteMapping
     public Result delete(Integer id){
-        System.out.println("根据ID删除部门: " + id);
+        log.info("根据ID删除部门: {} ", id);
         deptService.deleteById(id);
         return Result.success();
     }
@@ -38,7 +41,7 @@ public class DeptController {
      */
     @PostMapping
     public Result add(@RequestBody Dept dept) {
-        System.out.println("新增部门： " + dept);
+        log.info("新增部门：{}", dept);
         deptService.add(dept);
         return Result.success();
     }
@@ -47,7 +50,7 @@ public class DeptController {
      */
     @GetMapping("/{id}")
     public Result getInfo(@PathVariable Integer id){
-        System.out.println("根据ID查询部门： " + id);
+        log.info("根据ID查询部门：{}", id);
         Dept dept = deptService.getById(id);
         return Result.success(dept);
     }
@@ -56,7 +59,7 @@ public class DeptController {
      */
     @PutMapping
     public Result updateInfo(@RequestBody Dept dept){
-        System.out.println("修改部门： " + dept);
+        log.info("修改部门：{}", dept);
         deptService.update(dept);
         return Result.success();
     }
